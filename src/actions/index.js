40ac8ec940d05550261c9ef64f6493a9b7fda7db
foncyc-dlsl <alphonsus.cootauco@dlsl.edu.ph>
2018-01-24 16:35:28 +0800

    Wed Jan 24 Intermediate Redux: Middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,17 @@
-const API_KEY = '752e1d274a67ca3f10260fe0a1e18aea';
+import axios from 'axios';
 
+const API_KEY = '752e1d274a67ca3f10260fe0a1e18aea';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH WEATHER';
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},ph`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import ReduxPromise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+// npm install --save redux-promise
+// npm install --save axios
+// npm i --save react-sparklines@1.6.0
+// npm install --save lodash
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
